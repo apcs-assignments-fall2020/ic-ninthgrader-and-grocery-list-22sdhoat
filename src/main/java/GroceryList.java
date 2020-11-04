34985905092fa9d3546577c38bdd5561e452631a
22sdhoat <22sdhoat@athenian.org>
2020-11-04 03:28:56 +0000

    add 3 methods to grocery list and its respective client

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,39 @@
 public class GroceryList {
-    
-}
+    private String[] arr;
+
+    public GroceryList() {
+        this.arr = new String[10]; 
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                for (int x = 0; x < arr.length - 1; x++) {
+                    arr[i] = arr[x+1];
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        String str = "Grocery List: ";
+        for (int i = 0; i < this.arr.length - 1; i++) {
+            if (this.arr[i] != null){
+                str += this.arr[i] + ", ";
+            }
+            else if (this.arr[i+1] == null) {
+                break;
+            }
+        }
+        return str;
+    }
+}   
